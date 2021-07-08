52757a4dd0e2d80b3bc7b4959615974ee4a65fc8
jesuscardenascastro <44135302+jesuscardenascastro@users.noreply.github.com>
2021-07-07 20:31:37 -0500

    implementando métodos Subdivide y Contains

--- Octree/octree.js
+++ Octree/octree.js
@@ -17,7 +17,12 @@ class Box{
         this.d=d;
     }
     contains(point){
-
+        return (point.x >= (this.x - this.w) && 
+        point.x <= (this.x + this.w) && 
+        point.y >= (this.y - this.h) && 
+        point.y <= (this.y + this.h) &&
+        point.z >= (this.z - this.d) &&
+        point.z <= (this.z + this.d));
     }
     intersectc(box){
 
@@ -32,7 +37,33 @@ class Octree{
         this.divided=false;
     }
     subdivide(){
+        let x = this.box.x;
+        let y = this.box.y;
+        let z = this.box.z;
+        let w = this.box.w/2;
+        let h = this.box.h/2;
+        let d = this.box.d/2;
 
+        let nof = new Box(x-w,y-h,z-d,w,h,d); 
+        let nef = new Box(x+w,y-h,z-d,w,h,d);
+        let sof = new Box(x-w,y+h,z-d,w,h,d);
+        let sef = new Box(x+w,y+h,z-d,w,h,d);
+        let nob = new Box(x-w,y-h,z+d,w,h,d);
+        let neb = new Box(x+w,y-h,z+d,w,h,d);
+        let sob = new Box(x-w,y+h,z+d,w,h,d);
+        let seb = new Box(x+w,y+h,z+d,w,h,d);
+
+        this.sonNOF = new Octree(nof,this.capacity);
+        this.sonNEF = new Octree(nef,this.capacity);
+        this.sonSOF = new Octree(sof,this.capacity);
+        this.sonSEF = new Octree(sef,this.capacity);
+        this.sonNOB = new Octree(nob,this.capacity);
+        this.sonNEB = new Octree(neb,this.capacity);
+        this.sonSOB = new Octree(sob,this.capacity);
+        this.sonSEB = new Octree(seb,this.capacity);
+
+        alpha = alpha + 0.1;
+        this.divided = true;
     }
     insert(point){
 
